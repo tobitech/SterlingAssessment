a0472fa9c195637209d79adc3fc8f43a9fe5362e
Oluwatobi Omotayo <tobitech@ymail.com>
2020-03-16 12:15:50 +0100

    commit missing file

---
+++ SterlingAssessment/Utils/Identifiable/ClassIdentifiable.swift
@@ -0,0 +1,19 @@
+//
+//  ClassIdentifiable.swift
+//  SterlingAssessment
+//
+//  Created by Oluwatobi Omotayo on 16/03/2020.
+//  Copyright © 2020 Oluwatobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+protocol ClassIdentifiable: class {
+    static var reuseId: String { get }
+}
+
+extension ClassIdentifiable {
+    static var reuseId: String {
+        return String(describing: self)
+    }
+}
