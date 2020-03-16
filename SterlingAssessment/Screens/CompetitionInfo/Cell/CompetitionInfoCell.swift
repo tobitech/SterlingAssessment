df36740bd92d29f30dd1c759c3fd0b790b714fe3
Oluwatobi Omotayo <tobitech@ymail.com>
2020-03-16 15:20:37 +0100

    switch competition info with page scrolling and when segmented control selected index changed

---
+++ SterlingAssessment/Screens/CompetitionInfo/Cell/CompetitionInfoCell.swift
@@ -0,0 +1,18 @@
+//
+//  CompetitionInfoCell.swift
+//  SterlingAssessment
+//
+//  Created by Oluwatobi Omotayo on 16/03/2020.
+//  Copyright © 2020 Oluwatobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+class CompetitionInfoCell: UICollectionViewCell, NibIdentifiable & ClassIdentifiable {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- SterlingAssessment/Screens/CompetitionInfo/CompetitionInfoViewController.swift
+++ SterlingAssessment/Screens/CompetitionInfo/CompetitionInfoViewController.swift
@@ -20,10 +20,41 @@ class CompetitionInfoViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.title = " "
 
         navigationItem.title = viewModel.name
+        segmentedControl.addTarget(self, action: #selector(handleSegmentChanged(_:)), for: .valueChanged)
+        
+        setupCollectionView()
     }
     
     private func setupCollectionView() {
-        
+        collectionView.register(cellType: CompetitionInfoCell.self)
+        collectionView.isPagingEnabled = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
+    
+    // MARK: Action Methods
+    @objc func handleSegmentChanged(_ sender: UISegmentedControl) {
+        collectionView.scrollToItem(at: IndexPath(item: sender.selectedSegmentIndex, section: 0), at: .centeredHorizontally, animated: true)
+    }
+}
 
+extension CompetitionInfoViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withCellType: CompetitionInfoCell.self, forIndexPath: indexPath)
+        cell.backgroundColor = [UIColor.black, .blue, .orange][indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let index = Int(targetContentOffset.pointee.x / view.frame.width)
+        self.segmentedControl.selectedSegmentIndex = index
+    }
 }
